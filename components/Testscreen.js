67b90a33b0f5c37d9c701260ef55ddd9dff906e1
nghiadv09 <nghia1392001@gmail.com>
2023-02-01 23:12:55 +0700

    first commit

---
+++ components/Testscreen.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { Button, View, Text } from "react-native";
+export default function TestScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Test Screen</Text>
+    </View>
+  );
+}
